b79e267384cb500659433473e04e6a69f6d8dc26
luna9862 <lunaest26@gmail.com>
2024-09-16 14:41:31 -0400

    files werent added

--- src/main/java/org/carolina/bdd_project/BddProjectApplication.java
+++ src/main/java/org/carolina/bdd_project/BddProjectApplication.java
@@ -2,12 +2,14 @@ package org.carolina.bdd_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories("org.carolina.bdd_project.repository")
+@EntityScan("org.carolina.bdd_project.model")  // Add this if necessary
 public class BddProjectApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BddProjectApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BddProjectApplication.class, args);
+    }
 }
--- src/test/java/org/carolina/bdd_project/BddProjectApplicationTests.java
+++ src/test/java/org/carolina/bdd_project/BddProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BddProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
